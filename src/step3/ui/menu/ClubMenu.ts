e4b4ce190de80e3045fe08863692b79b02b38fd7
CodeAcademic <oficialjaloliddin@gmail.com>
2026-05-23 20:15:03 +0500

    step3'ni yakunladim

---
+++ src/step3/ui/menu/ClubMenu.ts
@@ -0,0 +1,72 @@
+import { question } from "readline-sync";
+import ClubConsole from "../console/ClubConsole";
+import ClubMemberShipMenu from "./ClubMembershipMenu";
+
+
+class ClubMenu {
+    clubMembershipMenu: ClubMemberShipMenu;
+    clubConsole: ClubConsole;
+
+    constructor() {
+        this.clubMembershipMenu = new ClubMemberShipMenu();
+        this.clubConsole = new ClubConsole();
+    }
+
+    showMenu(): void {
+        let inputNumber = 0;
+
+        while (true) {
+            this.displayMainMenu();
+            inputNumber = this.selectMenu();
+
+            switch (inputNumber) {
+                case 1:
+                    this.clubConsole.register();
+                    break;
+                case 2:
+                    this.clubConsole.find();
+                    break;
+                case 3:
+                    this.clubConsole.modify();
+                    break;
+                case 4:
+                    this.clubConsole.remove();
+                    break;
+                case 5:
+                    this.clubMembershipMenu.showMenu();
+                    break;
+                case 0:
+                    return;
+                default:
+                    console.log('Choose Again!');
+            }
+        }
+    }
+
+    displayMainMenu(): void {
+        console.log('.........................');
+        console.log(' [Travel Club menu]');
+        console.log('.........................');
+        console.log(' 1. Register');
+        console.log(' 2. Find');
+        console.log(' 3. Modify');
+        console.log(' 4. Remove');
+        console.log(' .........................');
+        console.log(' 0. Previous');
+        console.log('..........................');
+    }
+
+    selectMenu(): number {
+        const answer = question('Select number : ');
+        const menuNumber = parseInt(answer);
+
+        if (menuNumber >= 0 && menuNumber <= 5) {
+            return menuNumber;
+        } else {
+            console.log('it\'s a invalid number -> ' + menuNumber);
+            return -1;
+        }
+    }
+}
+
+export default ClubMenu;
